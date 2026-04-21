7a4d881d0c4f836ccc4eb1e458b6ecc17869b893
pc <pc@DESKTOP-2N68CP9>
2026-04-21 21:00:27 +0200

    test

---
+++ BNU-Student-Portal-Presentation/controllers/TestController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BNU_Student_Portal_Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+    }
+}
