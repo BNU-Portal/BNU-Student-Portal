525e09888da0d7c8d5b508ea876ab8a1f54faa8d
Saif Lotfy <154231473+sefffo@users.noreply.github.com>
2026-04-23 15:49:24 +0200

    chore: remove YourAPP template files

--- YourAPP-Services/Features/TestFeat/CreateTestFeatCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace YourAPP_Services.Features.TestFeat
-{
-    public record CreateTestFeatCommand(int id, string name) : IRequest<int>;
-}
