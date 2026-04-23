85562f608167fb4d5da9a00436c9c19d206be2e0
pc <pc@DESKTOP-2N68CP9>
2026-04-23 16:46:02 +0200

    Fix

--- BNU-Student-Portal-Domain/Entities/Auth/Address.cs
+++ BNU-Student-Portal-Domain/Entities/Auth/Address.cs
@@ -1,27 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace BNU_Student_Portal_Domain.Entities.Auth
+﻿namespace BNU_Student_Portal_Domain.Entities.Auth
 {
     public class Address
     {
-        public int Id { get; set; } //pk
+        public int Id { get; set; }
 
         public string City { get; set; } = default!;
-
         public string Street { get; set; } = default!;
-
         public string Country { get; set; } = default!;
+        public string? Apartment { get; set; }  // nullable
 
-        public string Apartment { get; set; } 
-
-
-
-        //navigational properties
-        public AppUser User { get; set; } = default!;
-        public string UserId { get; set; } = default!;
-
+        // Belongs to Guardian only
         public Guid GuardianId { get; set; }
         public Guardian Guardian { get; set; } = default!;
     }
