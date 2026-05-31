80437ccc196a3db597254733783e55dfa3c5452d
Saif Lotfy <154231473+sefffo@users.noreply.github.com>
2026-05-31 17:05:56 +0300

    fix: normalize CertificateType string before Enum.Parse in AuthProfile

--- BNU-Student-Portal-Services/MappingProfiles/AuthProfile.cs
+++ BNU-Student-Portal-Services/MappingProfiles/AuthProfile.cs
@@ -1,4 +1,4 @@
-﻿using AutoMapper;
+using AutoMapper;
 using BNU_Student_Portal_Domain.Entities.Auth;
 using BNU_Student_Portal_Shared_Library.DTO_s.Auth;
 using System;
@@ -13,20 +13,43 @@ namespace BNU_Student_Portal_Services.MappingProfiles
         {
             CreateMap<RegisterStudentDto, Student>()
                 .ForMember(dest => dest.CertificateType,
-                    opt => opt.MapFrom(src => Enum.Parse<CertificateType>(src.CertificateType, ignoreCase: true)))
-                .ForMember(dest => dest.AppUserId, opt => opt.Ignore())  // set manually after CreateAsync
-                .ForMember(dest => dest.AppUser, opt => opt.Ignore())
-                .ForMember(dest => dest.Id, opt => opt.Ignore());
+                    opt => opt.MapFrom(src => ParseCertificateType(src.CertificateType)))
+                .ForMember(dest => dest.AppUserId, opt => opt.Ignore())
+                .ForMember(dest => dest.AppUser,   opt => opt.Ignore())
+                .ForMember(dest => dest.Id,        opt => opt.Ignore());
 
             CreateMap<RegisterProfessorDto, Professor>()
                 .ForMember(dest => dest.AppUserId, opt => opt.Ignore())
-                .ForMember(dest => dest.AppUser, opt => opt.Ignore())
-                .ForMember(dest => dest.Id, opt => opt.Ignore());
+                .ForMember(dest => dest.AppUser,   opt => opt.Ignore())
+                .ForMember(dest => dest.Id,        opt => opt.Ignore());
 
             CreateMap<RegisterTADto, TeachingAssistant>()
                 .ForMember(dest => dest.AppUserId, opt => opt.Ignore())
-                .ForMember(dest => dest.AppUser, opt => opt.Ignore())
-                .ForMember(dest => dest.Id, opt => opt.Ignore());
+                .ForMember(dest => dest.AppUser,   opt => opt.Ignore())
+                .ForMember(dest => dest.Id,        opt => opt.Ignore());
+        }
+
+        /// <summary>
+        /// Accepts all of these (case-insensitive, spaces/underscores ignored):
+        ///   "Thanaweya Amma" | "ThanaweyaAama" | "thanaweya_amma"
+        ///   "IGCSE"
+        ///   "IB"
+        ///   "American High School Diploma" | "AmericanHighSchoolDiploma"
+        ///   "Other"
+        /// </summary>
+        private static CertificateType ParseCertificateType(string value)
+        {
+            var normalized = value.Replace(" ", "").Replace("_", "").ToLowerInvariant();
+
+            return normalized switch
+            {
+                "thanaweyaaama" or "thanaweyaamma" => CertificateType.ThanaweyaAama,
+                "igcse"                            => CertificateType.IGCSE,
+                "ib"                               => CertificateType.IB,
+                "americanhighschooldiploma"        => CertificateType.AmericanHighSchoolDiploma,
+                "other"                            => CertificateType.Other,
+                _                                  => Enum.Parse<CertificateType>(value, ignoreCase: true)
+            };
         }
     }
 }
